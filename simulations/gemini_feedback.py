fbba4f469d322eca0bc757eb09369d6f823e9fd2
Dale Henion <mail2daleh@gmail.com>
2025-05-31 08:15:34 -0400

    fixing trulens issues

--- simulations/gemini_feedback.py
+++ simulations/gemini_feedback.py
@@ -1,5 +1,9 @@
 import os
 import google.generativeai as genai
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
